10c532c2519a52901d04ecc2736f31e35110813c
Saif Uddin <admin@saifuddin.uk>
2024-01-25 16:34:57 +0000

    added apache and GNU license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,27 +1,4 @@
-// function to generate markdown for README
-const licenseDescription = (data) =>{
-  switch (data.license) {
-    case 'MIT':
-return `Copyright (c) 2024 ${data.github||'n/a'}
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
-
-    case ''||'The Unlicense':
-return `Copyright (C) 2017-2018 ${data.github||'n/a'} <${data.email||'n/a'}>
-
-This file is part of the ${data.title||'n/a'} project.
-
-The ${data.title||'n/a'} project can not be copied and/or distributed without the express
-permission of ${data.github||'n/a'} <${data.email||'n/a'}>.`
-
-    default:
-      return ''
-  } 
-}
+const licenseDescriptions = require("./licenseDescriptions")
 
 const deployedPage = (data)=> {
   return data.deployed ? `[Link to deployed web page.](${data.deployed})` : ''
@@ -63,7 +40,7 @@ ${data.usage||'n/a'}
 
 ## License
 ### ${data.license||'UNLICENSED'} License
-${licenseDescription(data)}
+${licenseDescriptions(data)}
 
 ## Credits
 
